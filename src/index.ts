cdb608e6a544786b29bf35961a94e8236ba32686
AnaMVB57 <amvillaba57@gmail.com>
2026-05-11 10:31:39 -0400

    feat: add middlewareLogResponse for non-OK response logging

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import express from "express";
+import { middlewareLogResponse } from "./app/middleware/middlewareLogResponses.js";
 
 export let app = express();
 const PORT = 8080;
 
+app.use(middlewareLogResponse);
 
 function handlerReadiness(req: Request, res: Response) {
   res.set("Content-Type", "text/plain; charset=utf-8");
